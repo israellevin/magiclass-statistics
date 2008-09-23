55cbb9b8de1705ed1cae14ec71109264955951fb
i <bildad@gmail.com>
2008-09-24 00:58:27 +0300

    Got matrix to work, no tabs, for real

--- magiclass.js
+++ magiclass.js
@@ -1,8 +1,6 @@
 var lang;
-var action;
 
 $(document).ready(function(){
-    action = ['', ''];
     // Get language from location params in the form "lang=xx"
     var loc = document.location.href;
     var idx = loc.indexOf('?');
@@ -17,32 +15,12 @@ $(document).ready(function(){
     $.getScript((lang || 'en') + '.js', function(){
         $('#menu td').each(function(){
             var s = this.innerHTML;
-            jQuery(this).addClass(s).click(function(){
-                var curJ = jQuery(this);
-                if(curJ.attr('class').substr(0, 2) == 'by'){
-                    action = [action[0], curJ];
-                }else{
-                    action = [curJ, action[1]];
-                }
-                    $('#types td').css('backgroundColor', 'blue');
-                    action = [s, action[1]];
-                } else if(curJ.hasClass('bys')){
-                    $('#byd td').css('backgroundColor', 'blue');
-                    action = [action[0], s];
-                }
-                curJ.css('backgroundColor', 'red');
+            jQuery(this).click(function(){
+                jQuery(this).css('backgroundColor', 'red').siblings('td').css('backgroundColor', 'white');
             });
         });
         $('body').css('direction', lang['direction']);
         $('#headline').html(lang['headline']);
         $("#mainMenu").html('asd');
-/*
-    var data = [];
-    for(var i = 0; i < 334; i += 5)
-        data.push([i, i * i]);
-    data = [data];
-
-    $.plot($("#placeholder"), data);
-*/
     });
 });
