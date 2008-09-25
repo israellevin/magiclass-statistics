4e3365829b512980e39180a2d9d7b6c1337fc288
i <bildad@gmail.com>
2008-09-25 20:22:39 +0300

    Resizable graph

--- magiclass.js
+++ magiclass.js
@@ -1,6 +1,40 @@
 var lang;
+var data;
+
+function setData()
+{
+    var d1 = [];
+    for (var i = 0; i < 14; i += 0.5)
+        d1.push([i, Math.sin(i)]);
+    var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
+    var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
+    data = [d1, d2, d3];
+}
+
+function setResizables()
+{
+    $('#attendanceGraph').resizable({
+		minWidth: 100,
+		minHeight: 100,
+        knobHandles: true,
+        autoHide: true,
+        stop: function(){
+            $.plot($("#attendanceGraph"), data);
+            $('#attendanceGraph').resizable('destroy');
+            setResizables();
+        },
+	});
+}
 
 $(document).ready(function(){
+
+    setData();
+
+    $.plot($("#attendanceGraph"), data);
+
+    setResizables();
+
+
     // Get language from location params in the form "lang=xx"
     var loc = document.location.href;
     var idx = loc.indexOf('?');
@@ -23,12 +57,5 @@ $(document).ready(function(){
         if(lang['direction'] == 'rtl'){
             $('.ui-tabs-nav').css('float', 'right');
         }
-            /*
-            jQuery(this).click(function(){
-                jQuery(this).css('backgroundColor', 'red').siblings('td').css('backgroundColor', 'white');
-            });
-        });
-        $('#headline').html(lang['headline']);
-        $("#mainMenu").html('asd');*/
     });
 });
