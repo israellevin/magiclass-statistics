036f426b08b0fc4b613219fde9ff3df701b24042
root <israellevin@fastmail.fm>
2009-03-13 16:04:44 +0200

    Nice filters running

--- data.js
+++ data.js
@@ -1,18 +1,37 @@
-function rnd(max) { return Math.floor(Math.random() * max) }
+lessons.push('08:15');
+lessons.push('09:30');
+lessons.push('10:30');
+lessons.push('11:15');
+lessons.push('13:30');
+lessons.push('14:15');
 
-lessons.push('0815');
-lessons.push('0930');
-lessons.push('1030');
-lessons.push('1115');
-lessons.push('1330');
-lessons.push('1415');
+classes.push('אל"ף');
+classes.push('Chem-101');
+classes.push('Auto-2');
+classes.push('10th');
+classes.push('Aleph 2');
 
-classes.push('ALEF ECHAD');
-classes.push('ALEF SHTAIM');
-classes.push('ALEF SHALOSH');
-classes.push('BET ECHAD');
-classes.push('BET SHTAIM');
-
+students.push({id: '039213554', name: 'Isra Lin', classes: [0]});
+students.push({id: '639634567', name: 'asdfasdf', classes: ['1']});
+students.push({id: '259463466', name: 'weqwefasdf', classes: ['1']});
+students.push({id: '349773473', name: 'asasdfasdf', classes: ['1']});
+students.push({id: '339333333', name: 'asfasdf', classes: ['1']});
+students.push({id: '239346345', name: 'wewfda', classes: ['0']});
+students.push({id: '259272456', name: 'zxv', classes: ['0']});
+students.push({id: '139523463', name: 'sedqwev', classes: ['2']});
+students.push({id: '149345132', name: 'asdferfxc', classes: ['2']});
+students.push({id: '139513455', name: 'eqwsvxzcv', classes: ['2']});
+students.push({id: '149513463', name: 'qwetdvdefg', classes: ['2']});
+students.push({id: '149333616', name: 'sadfadvv', classes: ['3']});
+students.push({id: '119354136', name: 'asfsdf', classes: ['3']});
+students.push({id: '619136413', name: 'asdfczvwet', classes: ['3']});
+students.push({id: '749134553', name: 'qwetqwfasdv', classes: ['3']});
+students.push({id: '119413466', name: 'asdfsdferw', classes: ['4']});
+students.push({id: '759747777', name: 'asfasdfertg', classes: ['4']});
+students.push({id: '139533345', name: 'fhgdtrhg', classes: ['4']});
+students.push({id: '159444444', name: 'ASDSASdsf', classes: ['4']});
+students.push({id: '449555256', name: 'fdsdfsadf', classes: ['4']});
+students.push({id: '449235256', name: '42fsadf', classes: ['4']});
 students.push({id: '033213554', name: 'Israel Levin', classes: [0]});
 students.push({id: '634634567', name: 'asdfasdf', classes: ['1']});
 students.push({id: '253463466', name: 'wetqwefasdf', classes: ['1']});
@@ -35,8 +54,12 @@ students.push({id: '154444444', name: 'ASDSCASdsf', classes: ['4']});
 students.push({id: '444555256', name: 'fdfsdfsadf', classes: ['4']});
 students.push({id: '444235256', name: '423dfsadf', classes: ['4']});
 
-/*
 for(var idx = 0; idx < 100; idx++) {
-    attendance.addItem(rnd(99999), rnd(lessons.length), rnd(classes.length), rnd(students.length), (rnd(10) == 0 ? null : rnd(1000)));
+    attendance.push({
+        date: mkDate(),
+        lesson: rnd(lessons.length),
+        clas: rnd(classes.length),
+        student: rnd(students.length),
+        time: rnd(10) == 0 ? null : rnd(1000)
+    });
 }
-*/
--- magiclass.js
+++ magiclass.js
@@ -6,7 +6,61 @@ var lessons = [];
 var classes = [];
 var students = [];
 
-function translate(dlang) {
+// Helper functions
+function rnd(max) { return Math.floor(Math.random() * max) }
+function mkDate() {
+    var d = new Date();
+    d.setDate(d.getDate() - rnd(5));
+    return d;
+}
+
+// Attendance data
+var attendance = {
+    full: [],
+    byStud: [],
+    current: new Date(),
+    semStart: new Date(2008, 8, 1),
+    name: false
+};
+
+attendance.push = function(line) {
+    var aIdx = this.full.push(line);
+    var sIdx = line['student'];
+    if(this.byStud.hasOwnProperty(sIdx)) {
+        this.byStud[sIdx].push(this.full[aIdx - 1]);
+    } else {
+        this.byStud[sIdx] = [this.full[aIdx - 1]];
+    }
+};
+
+attendance.xhtmlize = function() {
+    var xhtml = '';
+    var idx = 0;
+    jQuery.each(this.byStud, function(key, val) {
+        var student = students[key];
+        var idn = student.id;
+        var i = $('#idNumInp').val();
+        if((i.length > 2) && (idn.indexOf(i) == -1)) return true;
+        var name = student.name;
+        var i = $('#studNameInp').val().toLowerCase();
+        if((i.length > 2) && (name.toLowerCase().indexOf(i) == -1)) return true;
+        var missAt = [];
+        var missSince = [];
+        jQuery.each(this, function(key, val) {
+            if(this.date >= attendance.semStart) {
+                missSince.push(this);
+                if((attendance.current - this.date < 1000 * 60 * 60 * 24) && (attendance.current.getDate() == this.date.getDate())) {
+                    missAt.push(this);
+                }
+            }
+        });
+
+        xhtml += '<tr' + (idx % 2 == 0 ? ' class="altRow"' : '') + '><td>' + (++idx) + '</td><td>' + idn + '</td><td>' + name + '</td><td>' + missAt.length + '</td><td>' + missSince.length + '</td>';
+        });
+    $('#data tbody').html(xhtml);
+};
+
+function translate(dlang, callback) {
     $.getScript(dlang + '.js', function() {
         if(lang.direction) {
             if(lang.direction == 'rtl') {
@@ -16,9 +70,45 @@ function translate(dlang) {
             }
         };
         $('.translate').each(function(){
-
                 jQuery(this).text(lang[this.id] || this.id);
         });
+
+        // Save dates
+        var missAtDate = $('#missAtInp').datepicker('getDate');
+        var missSinceDate = $('#missSinceInp').datepicker('getDate');
+
+        // Date picker config
+        $('.datepicker').datepicker('setDate').datepicker(
+            'option', 'dateFormat',
+            lang['dateFormat'] || 'mm/dd/yy'
+        ).datepicker(
+            'option', 'dayNamesMin',
+            lang['days'] || ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
+        ).datepicker(
+            'option', 'firstDay',
+            ((lang['firstDay'] && parseInt(lang['firstDay'])) || 2) - 1
+        ).datepicker(
+            'option', 'isRTL',
+            (lang['direction'] && lang['direction'] == 'rtl') ? true : false
+        ).datepicker(
+            'option', 'monthNamesShort',
+            lang['months'] || ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        ).datepicker(
+            'option', 'nextText',
+            lang['next'] || 'next'
+        ).datepicker(
+            'option', 'prevText',
+            lang['prev'] || 'prev'
+        ).datepicker(
+            'option', 'firstDay',
+            ((lang['firstDay'] && parseInt(lang['firstDay'])) || 2) - 1
+        ).filter('#missAtInp').datepicker(
+            'setDate', missAtDate
+        ).end().filter('#missSinceInp').datepicker(
+            'setDate', missSinceDate
+        );
+
+        if(callback) callback();
     });
 }
 
@@ -32,9 +122,39 @@ $(document).ready(function() {
         var pair = params[idx].split('=');
         if(pair.length == 2 && pair[0] == 'lang') { lang = pair[1]; }
     }};
-    translate(lang || 'en');
 
-    // Load the data
-    $.getScript('data.js', function() {
+    // Bind date pickers
+    $('.datepicker').datepicker({
+        changeMonth: true,
+        changeYear: true,
+        duration: 'fast',
+        maxDate: 0,
+        showButtonPanel: true,
+        onSelect: function(str, dat) {
+            if(this.id == 'missAtInp') {
+                attendance.current = new Date(dat.currentYear, dat.currentMonth, dat.currentDay); 
+            } else if (this.id == 'missSinceInp') {
+                attendance.semStart = new Date(dat.currentYear, dat.currentMonth, dat.currentDay); 
+            }
+            attendance.xhtmlize();
+    }}).filter('#missAtInp').datepicker('setDate', attendance.current).end().filter('#missSinceInp').datepicker('setDate', attendance.semStart);
+
+
+    // Bind inputs
+    $('input:text').keyup(function(e) {
+        switch(this.id) {
+            case 'idNumInp':
+                if(e.which > 64 && e.which < 91) return false;
+                if(this.value.length > 9) return false;
+                break;
+        }
+        attendance.xhtmlize();
+    });
+
+    translate(lang || 'en', function() {
+        // Load the data
+        $.getScript('data.js', function() {
+            attendance.xhtmlize();
+        });
     });
 });
